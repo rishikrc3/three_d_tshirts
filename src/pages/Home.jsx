2b26a47be7e478e8625e0e0295db05995add48b4
Rishik Chakravarty <rishikrc3@gmail.com>
2023-06-29 17:20:42 +0530

    animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,6 +21,22 @@ const Home = () => {
               className="w-8 h-8 object-contain"
             />
           </motion.header>
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                LET'S <br className="xl:block hidden" /> DO IT.
+              </h1>
+            </motion.div>
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            >
+              <p className="max-w-md font-normal text-gray-600 text-base">
+              Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your imagination</strong>{" "} and define your own style.
+              </p>
+
+            </motion.div>
+            </motion.div>
           </motion.section>)}
     </AnimatePresence>
   )
