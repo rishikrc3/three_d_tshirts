6ca8e5dda4144f8658b9c52f7fa1d2ce88426bd2
Rishik Chakravarty <rishikrc3@gmail.com>
2023-06-29 23:00:49 +0530

    t_shirt_added

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import {Canvas} from '@react-three/fiber'
+import { Environment, Center } from '@react-three/drei'
 
-const Canvas = () => {
+
+
+import Backdrop from './Backdrop'
+import Shirt from './Shirt'
+import CameraRig from './CameraRig'
+const CanvasModel = () => {
   return (
-    <div>
-      Canvas
-    </div>
+    <Canvas>
+       <ambientLight intensity={0.5} />
+       <Center>
+          <Shirt />
+        </Center>
+    </Canvas>
   )
 }
 
-export default Canvas
+export default CanvasModel
