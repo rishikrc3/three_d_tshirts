a19ec1985d1894f81121085d926d62e64b40440f
Rishik Chakravarty <rishikrc3@gmail.com>
2023-06-29 19:56:55 +0530

    button_done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {motion, AnimatePresence} from 'framer-motion'
 import { useSnapshot } from 'valtio'
 import state from '../store';
+import { CustomButton } from '../components';
 import {
     headContainerAnimation,
     headContentAnimation,
@@ -34,7 +35,12 @@ const Home = () => {
               <p className="max-w-md font-normal text-gray-600 text-base">
               Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your imagination</strong>{" "} and define your own style.
               </p>
-
+            <CustomButton
+            type="filled"
+            title="Customize It"
+            handleClick={() => state.intro = false}
+            customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+            />
             </motion.div>
             </motion.div>
           </motion.section>)}
