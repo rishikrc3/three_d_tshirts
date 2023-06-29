202742c2b54f4fd6fb0f6e30d97283d91ffe764c
Rishik Chakravarty <rishikrc3@gmail.com>
2023-06-29 22:04:39 +0530

    tabs_

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -19,7 +19,25 @@ const Customizer = () => {
     <AnimatePresence>
      {!snap.intro&&(
       <>
-      Jai Kisan
+      <motion.div
+            key="custom"
+            className="absolute top-0 left-0 z-10"
+            {...slideAnimation('left')}
+          >
+              <div className="flex items-center min-h-screen">
+              <div className="editortabs-container tabs">
+                {EditorTabs.map((tab) => (
+                  <Tab 
+                    key={tab.name}
+                    tab={tab}
+                    handleClick={() => {}}
+                  />
+                ))}
+
+                
+              </div>
+            </div>
+            </motion.div>
       </>
      )}
     </AnimatePresence>
