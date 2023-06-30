cbdcf4a655ba7e3995f9dd8acf2c5bdfe579dac0
Rishik Chakravarty <rishikrc3@gmail.com>
2023-07-01 03:22:02 +0530

    tabs_

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -13,7 +13,7 @@ const CanvasModel = () => {
     gl={{ preserveDrawingBuffer: true }}
     className="w-full max-w-full h-full transition-all ease-in">
        <ambientLight intensity={0.5} />
-       {/* <Environment preset="city" /> */}
+       <Environment preset="city" />
 
        <CameraRig>
        <Backdrop />
