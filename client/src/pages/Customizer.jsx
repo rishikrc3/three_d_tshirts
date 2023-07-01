1ba62a5dbbfcdf40b5bc1f2ed80f0430e45f0b2f
Rishik Chakravarty <rishikrc3@gmail.com>
2023-07-01 20:10:02 +0530

    tab_buttons

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -127,8 +127,8 @@ const Customizer = () => {
                 key={tab.name}
                 tab={tab}
                 isFilterTab
-                isActiveTab=""
-                handleClick={() => {}}
+                isActiveTab={activeFilterTab[tab.name]}
+                handleClick={() =>  handleActiveFilterTab(tab.name)}
               />
             ))}
           </motion.div>
