1c8f2edc312bcaedde5514e36c0ffcf7c644ccea
Rishik Chakravarty <rishikrc3@gmail.com>
2023-07-02 03:23:41 +0530

    final_comit

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -4,20 +4,19 @@ import { useSnapshot } from 'valtio';
 import { useFrame } from '@react-three/fiber';
 import { Decal, useGLTF, useTexture } from '@react-three/drei';
 
-
 import state from '../store';
 
-
 const Shirt = () => {
-    const snap = useSnapshot(state);
-    const { nodes, materials } = useGLTF('/shirt_baked.glb');
-  
-    const logoTexture = useTexture(snap.logoDecal);
-    const fullTexture = useTexture(snap.fullDecal);
-    useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta));
+  const snap = useSnapshot(state);
+  const { nodes, materials } = useGLTF('/shirt_baked.glb');
+
+  const logoTexture = useTexture(snap.logoDecal);
+  const fullTexture = useTexture(snap.fullDecal);
+
+  useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta));
 
   const stateString = JSON.stringify(snap);
-    
+
   return (
     <group key={stateString}>
       <mesh
@@ -35,19 +34,19 @@ const Shirt = () => {
             map={fullTexture}
           />
         )}
-         {snap.isLogoTexture && (
+
+        {snap.isLogoTexture && (
           <Decal 
             position={[0, 0.04, 0.15]}
             rotation={[0, 0, 0]}
             scale={0.15}
             map={logoTexture}
-             //map-anisotropy={16}
-             depthTest={false}
-             depthWrite={true}
+            //map-anisotropy={16}
+            depthTest={false}
+            depthWrite={true}
           />
         )}
-        
-        </mesh>
+      </mesh>
     </group>
   )
 }
--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,20 +1,18 @@
-import React,{useState, useEffect}from 'react'
-import { AnimatePresence, motion } from 'framer-motion'
-import { useSnapshot } from 'valtio'
+import React, { useState, useEffect } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useSnapshot } from 'valtio';
 
 import config from '../config/config';
 import state from '../store';
-import {download} from '../assets'
-import {downloadCanvasToImage, reader} from '../config/helpers'
-import {EditorTabs, FilterTabs, DecalTypes} from '../config/constants'
-import { fadeAnimation,slideAnimation } from '../config/motion';
+import { download } from '../assets';
+import { downloadCanvasToImage, reader } from '../config/helpers';
+import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
+import { fadeAnimation, slideAnimation } from '../config/motion';
 import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
-
-
-
 
 const Customizer = () => {
   const snap = useSnapshot(state);
+
   const [file, setFile] = useState('');
 
   const [prompt, setPrompt] = useState('');
@@ -25,24 +23,57 @@ const Customizer = () => {
     logoShirt: true,
     stylishShirt: false,
   })
-   // show tab content depending on the activeTab
+
+  // show tab content depending on the activeTab
   const generateTabContent = () => {
     switch (activeEditorTab) {
       case "colorpicker":
         return <ColorPicker />
       case "filepicker":
         return <FilePicker
-        file={file}
-        setFile={setFile}
-        readFile={readFile}
+          file={file}
+          setFile={setFile}
+          readFile={readFile}
         />
       case "aipicker":
         return <AIPicker 
+          prompt={prompt}
+          setPrompt={setPrompt}
+          generatingImg={generatingImg}
+          handleSubmit={handleSubmit}
         />
       default:
         return null;
     }
   }
+
+  const handleSubmit = async (type) => {
+    if(!prompt) return alert("Please enter a prompt");
+
+    try {
+      setGeneratingImg(true);
+
+      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          prompt,
+        })
+      })
+
+      const data = await response.json();
+
+      handleDecals(type, `data:image/png;base64,${data.photo}`)
+    } catch (error) {
+      alert(error)
+    } finally {
+      setGeneratingImg(false);
+      setActiveEditorTab("");
+    }
+  }
+
   const handleDecals = (type, result) => {
     const decalType = DecalTypes[type];
 
@@ -52,7 +83,6 @@ const Customizer = () => {
       handleActiveFilterTab(decalType.filterTab)
     }
   }
-
 
   const handleActiveFilterTab = (tabName) => {
     switch (tabName) {
@@ -77,6 +107,7 @@ const Customizer = () => {
       }
     })
   }
+
   const readFile = (type) => {
     reader(file)
       .then((result) => {
@@ -84,30 +115,32 @@ const Customizer = () => {
         setActiveEditorTab("");
       })
   }
+
   return (
     <AnimatePresence>
-     {!snap.intro&&(
-      <>
-      <motion.div
+      {!snap.intro && (
+        <>
+          <motion.div
             key="custom"
             className="absolute top-0 left-0 z-10"
             {...slideAnimation('left')}
           >
-              <div className="flex items-center min-h-screen">
+            <div className="flex items-center min-h-screen">
               <div className="editortabs-container tabs">
                 {EditorTabs.map((tab) => (
                   <Tab 
                     key={tab.name}
                     tab={tab}
-                    handleClick={() => {setActiveEditorTab(tab.name)}}
+                    handleClick={() => setActiveEditorTab(tab.name)}
                   />
                 ))}
+
                 {generateTabContent()}
-                
               </div>
             </div>
-            </motion.div>
-            <motion.div
+          </motion.div>
+
+          <motion.div
             className="absolute z-10 top-5 right-5"
             {...fadeAnimation}
           >
@@ -118,6 +151,7 @@ const Customizer = () => {
               customStyles="w-fit px-4 py-2.5 font-bold text-sm"
             />
           </motion.div>
+
           <motion.div
             className='filtertabs-container'
             {...slideAnimation("up")}
@@ -128,12 +162,12 @@ const Customizer = () => {
                 tab={tab}
                 isFilterTab
                 isActiveTab={activeFilterTab[tab.name]}
-                handleClick={() =>  handleActiveFilterTab(tab.name)}
+                handleClick={() => handleActiveFilterTab(tab.name)}
               />
             ))}
           </motion.div>
-      </>
-     )}
+        </>
+      )}
     </AnimatePresence>
   )
 }
